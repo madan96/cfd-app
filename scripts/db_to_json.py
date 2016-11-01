e45934472a65e658aa70650f3742b3c40377657b
madan96 <rishabhmadan96@gmail.com>
2016-11-01 23:36:02 +0530

    Extract market price of essentials

--- scripts/db_to_json.py
+++ scripts/db_to_json.py
@@ -6,13 +6,13 @@ import json
 
 def extract_data():
 	data = {}
-	open('data.txt', 'w').close()
+	open('newsdata.txt', 'w').close()
 	table_service = TableService(account_name='newsaggregate', account_key='Im/sg8ePug9VWN14eZNbrMN10QCQC6s+spgO9JuYSRwa2wWwe1SrPQZ7nu+nEdt+dz/Mb/sBfIh3SUUU20QloQ==')
 	for i in range(1,11):
 		task = table_service.get_entity('newstable', 'newstagline', str(i))
 		data['headline'] = task.taglines
 		data['news'] = task.news
-		with open('data.txt', 'a') as outfile:
+		with open('newsdata.txt', 'a') as outfile:
 			json.dump(data, outfile, indent=4, sort_keys=True, separators=(',', ':'))
 
 def main():
---
+++ scripts/prices_to_db.py
@@ -0,0 +1,42 @@
+import urllib2
+import re
+from azure.storage.table import TableService, Entity
+from bs4 import BeautifulSoup
+
+base_url = 'http://kisaankranti.com/'
+params = ['fertilizer', 'pesticide', 'seeds']
+
+def extract_products():
+	table_service = TableService(account_name='pricekeeper', account_key='++V/62CPdKa1MaTiwYVzKWCUpXUfkWZSBJgO4SI6g/NIZ1GkBRixXWvguYxVvb/dpFMkxoI9SYDxwTTS/jTmqw==')
+	table_service.create_table('pricetable')
+	task = Entity()
+	k=1
+	for param in params:	
+		url = base_url + param + '.html'
+		bd = urllib2.Request(url, headers={'User-Agent' : "Magic Browser"}) 
+		req = urllib2.urlopen(bd)
+		data = BeautifulSoup(req.read(),"lxml")
+		details_area = data.find_all("div",{"class":"details-area"})
+		products = [] 
+		prices = []
+		for detail in details_area:
+			products.append(str(detail.find("h2", {"class":"product-name"}).text))
+			prices.append(detail.find("span", id=lambda x: x and x.startswith('product-')).text)
+
+		for i in range(0,len(prices)):
+			prices[i] = re.sub('\s+','',prices[i])
+			prices[i] = str(re.sub(r'[^\x00-\x7F]+','', prices[i]))
+
+		m=0
+		for product in products:
+			task = {'PartitionKey': 'pricetab', 'RowKey': str(k), 'producttype' : param, 'productname' : str(product), 'pricet' : prices[m]}
+			table_service.insert_or_replace_entity('pricetable', task)
+			m+=1
+			k+=1
+			
+
+def main():
+	extract_products()
+
+if __name__ == '__main__':
+	main()
